605c17baea57331313176b258682e8ef4ee2e4a2
Matthew Knight <mattlknight@gmail.com>
2019-03-23 18:42:42 -0400

    Add debug statements

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,14 @@ fn main() -> Result<(), Box<std::error::Error>> {
     dotenv::dotenv().ok();
     env_logger::init();
 
+    debug!("Pcap::new()");
     let mut pcap = Pcap::new();
     let devices = pcap.get_device_list()?;
     for device in devices.iter() {
         debug!("device: {:?}", device);
     }
 
+    debug!("main() -> OK");
     Ok(())
 }
 
